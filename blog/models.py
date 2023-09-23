4b487665aa613020350665a89f60755fb8cd7f0e
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-09-23 14:56:06 -0400

    add app blog

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=200, verbose_name='title')
+    content = models.TextField(verbose_name='content')
+    image = models.ImageField(upload_to='blog_images/', verbose_name='image')
+    views = models.PositiveIntegerField(default=0, verbose_name='views')
+    pub_date = models.DateTimeField(default=timezone.now, verbose_name='date_of_publication')
+
+    def __str__(self):
+        return self.title
