ead305e5b398830fb24467009a048505d9ad1290
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-09-24 09:15:25 -0400

    add settings for cash

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,10 @@ CRONJOBS = [
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
