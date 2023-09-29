bd5bf5f29fa5f436cc6a6e66d97836c0da8d6973
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-09-29 07:30:05 -0400

    пересоздал базу данных

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,12 +6,12 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='skay.user.11@icloud.com',
+            email='romakondratiuk01@gmail.com',
             first_name='Admin',
             last_name='SkyPro',
             is_staff=True,
             is_superuser=True
         )
 
-        user.set_password('Aa010203')
+        user.set_password('roma_2001')
         user.save()
