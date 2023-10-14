34735833284c4831db4cfcaea9c6af16ff3d702f
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-10-14 14:16:03 -0400

    update profile

--- users/forms.py
+++ users/forms.py
@@ -21,7 +21,7 @@ class UserProfileForm(UserChangeForm):
         model = User
         fields = ('email', 'first_name', 'last_name', 'avatar')
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-            self.fields['password'].widget = forms.HiddenInput()
+        self.fields['password'].widget = forms.HiddenInput()
